e02181a5dc73ef73670b27a9d471e42f6ec6d93c
Aarya-1011 <97793697+Aarya-1011@users.noreply.github.com>
2023-01-27 21:48:58 +0530

    Create Steps of 5.c

---
+++ Steps of 5.c
@@ -0,0 +1,13 @@
+//Write a C program to print the multiples of 5 between the first two digit number and first 3 digit number//
+
+#include <stdio.h>
+
+
+int main() {
+    int i;
+    for (i=15;i<100;i=i+5)
+    {
+        printf("%d\n",i);
+     }
+    return 0;
+}
