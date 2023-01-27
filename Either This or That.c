23a798808da05f1f966a0713f664cede5a35389e
Aarya-1011 <97793697+Aarya-1011@users.noreply.github.com>
2023-01-27 21:31:21 +0530

    Create Either This or That.c

---
+++ Either This or That.c
@@ -0,0 +1,17 @@
+//Input an integer which is the limit, say n. Write a C program to print all the numbers till the limit n that are divisible by either 2 and 3 or divisible by all 2,3 and 5.//
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,i=0;
+    scanf("%d",&a);
+    for (i=0;i<=a;i++)
+    if (i%2==0 && i%3==0)
+        printf("%d\n",i);
+    else if (i%2==0 && i%3==0 && i%5==0)
+        printf("%d\n",i);
+    return 0;
+}
