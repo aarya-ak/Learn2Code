68c8e9469d561f53987e7d7f8e948a17a78667d7
Aarya-1011 <97793697+Aarya-1011@users.noreply.github.com>
2023-01-30 12:14:28 +0530

    Update Hello world.c

--- Hello world.c
+++ Hello world.c
@@ -1,3 +1,5 @@
+//Create a program to display "Hello World" and "Welcome to Learn to code 2022" in separate lines with a horizontal tab space between the words "Hello" and "world". please mind that the problem is case sensitive.
+
 #include<studio.h>
 int main() {
     printf("Hello World\n");
