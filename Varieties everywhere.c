b886c40f395107dbb0ecacdcfb887fead1de6323
Aarya-1011 <97793697+Aarya-1011@users.noreply.github.com>
2023-01-30 11:24:32 +0530

    Create Varieties everywhere.c

---
+++ Varieties everywhere.c
@@ -0,0 +1,15 @@
+//Write a program to enter values of different data types and to display them
+
+#include <stdio.h>
+int main() {
+    int a;
+    char b;
+    float c;
+    scanf("%d",&a);
+    printf("%d",a);
+    scanf("\n%c",&b);
+    printf("\n%c",b);
+    scanf("\n%f",&c);
+    printf("\n%f",c);
+    return 0;
+}
