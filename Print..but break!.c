4d759d4e0168a6dd67e636a98dc6070b81288ff7
Aarya-1011 <97793697+Aarya-1011@users.noreply.github.com>
2023-01-30 11:21:47 +0530

    Create Print..but break!.c

---
+++ Print..but break!.c
@@ -0,0 +1,8 @@
+//Create a program to display "Hello World" and "Welcome to Learn to code 2022" in separate lines with a horizontal tab space between the words "Hello" and "world". Please mind that the problem is case sensitive.
+
+#include <stdio.h>
+int main() {
+     printf("Hello World\n");
+     printf("Welcome to Learn to code 2022");
+     return 0;
+}
