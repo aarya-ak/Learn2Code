0563f0f63b36e723a0716d4724cbe48159322c4c
Aarya Karunakaran <97793697+Aarya-1011@users.noreply.github.com>
2023-04-23 11:43:18 +0530

    Update Confusing Calculator.c

--- Confusing Calculator.c
+++ Confusing Calculator.c
@@ -1,12 +1,8 @@
-//Shweta wants to prank her friends using a 'confusing calculator'. After inputting two numbers, When the user gives an input which is an operator'#' it should perform addition Addition,'@' for Subtraction, '&' for Multiplication, and '%' for Division.
-
+/*Shweta wants to prank her friends using a 'confusing calculator'. After inputting two numbers, When the user gives an input which is an operator'#' it should perform addition Addition,'@' for Subtraction, '&' for Multiplication, and '%' for Division.
 When the case of Division is chosen, swap Dividend and Divisor during the division, and while Subtraction swap the first number with second number.
-
 When divisor is 0, print "Not Defined" and exit from the program.
-
 If any other character other than the specified operands are inputted, program should print "Error 5 : Input Error".
-
-Using switch statements, write a C program to create such a calculator.//
+Using switch statements, write a C program to create such a calculator.*/
 
 
 #include <stdio.h>
